122ddd8ba88fc534648d3066d9cd23dde1e35c28
Jaryd Meek <jarydmeek@gmail.com>
2025-11-30 17:49:58 -0700

    non-relative config

--- bumprr/config.py
+++ bumprr/config.py
@@ -6,13 +6,13 @@ def get_config():
     global _config
     if _config is None:
         try:
-            with open("config/config.toml", "rb") as config_file:
+            with open("/config/config.toml", "rb") as config_file:
                 _config = tomllib.load(config_file)
         except FileNotFoundError:
-            print("Config file not found. Creating a default config/config.toml... Please edit it and restart the application according to the README instructions.")
+            print("Config file not found. Creating a default /config/config.toml... Please edit it and restart the application according to the README instructions.")
             with open("config-example.toml", "rb") as example_file:
                 example_content = example_file.read()
-            with open("config/config.toml", "wb") as config_file:
+            with open("/config/config.toml", "wb") as config_file:
                 config_file.write(example_content)
             exit(1)
     return _config
